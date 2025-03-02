d3ba2dec01e117a5d41a685dfb3d417d050c5d83
sadafes <sadafesmaeili@yahoo.com>
2025-03-02 13:03:35 +0300

    final update

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ const priceElement3 = document.getElementById('price3');
 toggleBtn.addEventListener('click', function() {
     toggleBtn.classList.toggle('active');
     if (toggleBtn.classList.contains('active')) {
+        priceElement1.textContent = '19.99';
+        priceElement2.textContent = '29.99';
+        priceElement3.textContent = '39.99';
 
+    } else {
         priceElement1.textContent = '199.99';
         priceElement2.textContent = '299.99';
         priceElement3.textContent = '399.99';
 
-    } else {
-
-        priceElement1.textContent = '19.99';
-        priceElement2.textContent = '29.99';
-        priceElement3.textContent = '39.99';
     }
 });
